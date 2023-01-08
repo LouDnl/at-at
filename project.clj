1a7f12d56077e85dfc64e2af008cc9a1fa52cca8
LouDnl <thierry@smeekes.nl>
2023-01-08 16:33:34 +0100

    Update version to 1.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject overtone/at-at "1.2.0"
+(defproject overtone/at-at "1.4.0-SNAPSHOT"
   :description "Ahead-of-time function scheduler"
+  :url "https://github.com/LouDnl/at-at"
   :dependencies [[org.clojure/clojure "1.3.0"]])
