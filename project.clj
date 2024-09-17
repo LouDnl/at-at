78d5208277ada7a0da45374b0cd95da1613a9d7f
loudnl <LouDnl@users.noreply.github.com>
2024-09-17 16:07:22 +0200

    Add Clojure 1.12.0 to unittest profiles

--- project.clj
+++ project.clj
@@ -15,5 +15,7 @@
              :1.10.3 {:dependencies [[org.clojure/clojure "1.10.3"]]}
              :1.11   {:dependencies [[org.clojure/clojure "1.11.0"]]}
              :1.11.1 {:dependencies [[org.clojure/clojure "1.11.1"]]}
-             :alpha  {:dependencies [[org.clojure/clojure "1.12.0-alpha1"]]}}
+             :1.12.0 {:dependencies [[org.clojure/clojure "1.12.0"]]}
+             ;:alpha  {:dependencies [[org.clojure/clojure "1.12.0-alpha1"]]}
+             }
   :aliases  {"all" ["with-profile" "+1.6:+1.7:+1.8:+1.9:+1.10:+1.10.1:+1.10.2:+1.10.3+:1.11:+1.11.1:+alpha"]})
